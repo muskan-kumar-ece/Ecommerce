0fe4820c675833bbf13e5acbeab1a8dbe52ac384
Muskan kumar <muskankumar7842@gmail.com>
2026-03-02 01:14:36 +0530

    Delete Backend directory

--- Backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/v1/auth/", include("users.urls")),
-    path("api/v1/products/", include("products.urls")),
-    path("api/v1/orders/", include("orders.urls")),
-]
